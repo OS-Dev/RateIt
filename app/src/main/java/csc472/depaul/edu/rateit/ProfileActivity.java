30fb0d22ab4394ee0603af456bbd2c4f12753009
aliyaho24 <aliyaho24@github.com>
2018-11-19 18:11:54 -0600

    updated profile tabbed profile page

--- app/src/main/java/csc472/depaul/edu/rateit/ProfileActivity.java
+++ app/src/main/java/csc472/depaul/edu/rateit/ProfileActivity.java
@@ -1,8 +1,7 @@
 package csc472.depaul.edu.rateit;
 
+
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -12,83 +11,20 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.net.Uri;
+
 
 import android.widget.TextView;
 
-public class ProfileActivity extends AppCompatActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+public class ProfileActivity extends AppCompatActivity
+        implements ProfileFragment.OnFragmentInteractionListener,
+                    FavoritesFragment.OnFragmentInteractionListener,
+                    RatedFragment.OnFragmentInteractionListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_profile, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    public void onFragmentInteraction(Uri uri){}
 
     /**
      * A placeholder fragment containing a simple view.
@@ -118,34 +54,76 @@ public class ProfileActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_profile2, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private final String LOG_TAG = ProfileActivity.class.getSimpleName();
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+    // Titles of the individual pages (displayed in tabs)
+    private final String[] PAGE_TITLES = new String[] {
+            "Profile",
+            "Rated Items",
+            "Favorites"
+    };
 
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
+    // The fragments that are used as the individual pages
+    private final Fragment[] PAGES = new Fragment[] {
+            new ProfileFragment(),
+            new RatedFragment(),
+            new FavoritesFragment()
+    };
 
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
+    // The ViewPager is responsible for sliding pages (fragments) in and out upon user input
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        // Set the Toolbar as the activity's app bar (instead of the default ActionBar)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Connect the ViewPager to our custom PagerAdapter. The PagerAdapter supplies the pages
+        // (fragments) to the ViewPager, which the ViewPager needs to display.
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
+        // Connect the tabs with the ViewPager (the setupWithViewPager method does this for us in
+        // both directions, i.e. when a new tab is selected, the ViewPager switches to this page,
+        // and when the ViewPager switches to a new page, the corresponding tab is selected)
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(mViewPager);
     }
+
+
+/* PagerAdapter for supplying the ViewPager with the pages (fragments) to display. */
+public class MyPagerAdapter extends FragmentPagerAdapter {
+
+    public MyPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PAGES[position];
+    }
+
+    @Override
+    public int getCount() {
+        return PAGES.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return PAGE_TITLES[position];
+    }
+
+}
+
 }

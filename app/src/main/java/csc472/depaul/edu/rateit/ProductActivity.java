876f5dc86343341db526ca1ddedf428b288ceb85
Ozzy <osmel.savon@yahoo.com>
2018-11-20 12:32:12 -0600

    Updated Product Activity

--- app/src/main/java/csc472/depaul/edu/rateit/ProductActivity.java
+++ app/src/main/java/csc472/depaul/edu/rateit/ProductActivity.java
@@ -1,5 +1,6 @@
 package csc472.depaul.edu.rateit;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,12 +11,12 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 
-public class ProductActivity extends AppCompatActivity {
+public class ProductActivity extends AppCompatActivity{
 
+    //private Product product = new Product("Malort", -800003,"Best drink there is!", 1, 5);
     private Product product;
-
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
 
@@ -27,28 +28,22 @@ public class ProductActivity extends AppCompatActivity {
                 startActivity(new Intent(getUserActivity(), ProfileActivity.class));
             }
         });
-
             //Get Intent from Home of Profile
             Intent intent = getIntent();
             //Create product from parcel
             product = intent.getParcelableExtra("NEW_PRODUCT_INTENT");
-            //Gather data for display
-            String productName = product.getProductName();
-            int imgSrc = product.getImgSrc();
-            String productDescription = product.getProductDescription();
-            Integer avgRating = product.getAvgRating();
             //Display product name
-            final TextView productTextView = findViewById(R.id.product_name);
-            productTextView.setText(productName);
+            TextView productTextView = findViewById(R.id.product_name);
+            productTextView.setText(product.getProductName());
             //Display product image
-            final ImageView imgView = findViewById(R.id.product_img);
-            imgView.setImageDrawable(getDrawable(imgSrc));
+            ImageView imgView = findViewById(R.id.product_img);
+            imgView.setImageDrawable(getDrawable(product.getImgSrc()));
             //Display product description
-            final TextView productDescView = findViewById(R.id.product_description);
-            productDescView.setText(productDescription);
+            TextView productDescView = findViewById(R.id.product_description);
+            productDescView.setText(product.getProductDescription());
             //Display rating
-            final RatingBar ratingView = findViewById(R.id.ratingBar);
-            ratingView.setNumStars(avgRating);
+            RatingBar ratingView = findViewById(R.id.ratingBar);
+            ratingView.setNumStars(product.getAvgRating());
 
     }
 
